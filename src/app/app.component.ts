2fff6116a261e1d540f70a24520d8ddc920b09ec
Abhishek <choudharyabhishek162@gmail.com>
2022-01-04 06:07:02 +0530

    adding welcome message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Day2DayList';
+  message = 'Welcome to one of a kind app';
+
 }
