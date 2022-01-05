46e2e81592a3bd44e857ee8dc54ed3575b243815
Abhishek <choudharyabhishek162@gmail.com>
2022-01-05 05:53:57 +0530

    comparing welcome with java

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+//package name
 
+//import org.springframework.boot.SpringApplication;
+import { Component, OnInit } from '@angular/core';
+//import { AppComponent } from '../app.component';
+
+// @ComponentScane(value="package")
+//java annotation ==> javascript decorators
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+//public class ab implements interface
+//public ==> export
 export class WelcomeComponent implements OnInit {
 
+  //string message
+  message = 'welcome home'
+
+  //public ab(){}
   constructor() { }
 
+  //void method
   ngOnInit(): void {
+    // this.message = 5
+    console.log(this.message)
   }
 
 }
+
+// export class class1{}
