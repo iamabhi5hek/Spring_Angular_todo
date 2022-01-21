09aac64932ccc9cad8f87570d82ebda0c81bdf35
Abhishek <choudharyabhishek162@gmail.com>
2022-01-21 23:07:14 +0530

    adding authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +16,16 @@ export class LoginComponent implements OnInit {
 
   //dependency injection of router
   //will be avaulable as member variable
-  constructor(private router : Router) { }
+  constructor(private router : Router, 
+    private hardcodedAUthenticationService : HardcodedAuthenticationService) { }
 
   ngOnInit(): void {
   }
 
   handleLogin(){
     // console.log(this.username)
-    if(this.username === "username" && this.password === 'dummy'){
+    // if(this.username === "username" && this.password === 'dummy'){
+      if(this.hardcodedAUthenticationService.authenticate(this.username, this.password)){
       //redirect to welcome page so need to use instance of router so will use dependecy injection
       //need to declare as constructor argument
       this.router.navigate(['welcome', this.username])
