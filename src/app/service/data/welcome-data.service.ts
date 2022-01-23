b00f9ab4022c3bba74a42cbadec850402ff4e5e0
Abhishek <choudharyabhishek162@gmail.com>
2022-01-23 17:32:42 +0530

    connecting backend with frontend

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+executeHelloWorldBeanService(){
+  return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
+  // console.log("Executing executeHelloWorldBeanService")
+}
+
+}
+
+export class HelloWorldBean{
+
+  constructor(public message:String){
+
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,6 +3,7 @@
 //import org.springframework.boot.SpringApplication;
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 //import { AppComponent } from '../app.component';
 
 // @ComponentScane(value="package")
@@ -20,17 +21,31 @@ export class WelcomeComponent implements OnInit {
   //string message
   message = 'welcome home'
   name = ''
+  welcomeMessageFromService=''
 
   //public ab(){}
   //injecting name dependency
   //activated route
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute,
+    private service : WelcomeDataService) { }
 
   //void method
   ngOnInit(): void {
     // this.message = 5
     console.log(this.message) 
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMesage(){
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(  //async call , no response will be back 
+      response => this.handleSuccessfulResponse(response)
+      // response => console.log(response.message)
+    );
+  }
+
+  handleSuccessfulResponse(response: any){
+    this.welcomeMessageFromService = response.message
   }
 
 }
